5360bc513ea318b09185c9f009de12c8643397a3
Dmitriy Voronin <devoronin@gmail.com>
2021-04-25 10:50:11 +0300

    Initial Commit

---
+++ RootNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RootNavigation
+//
+//  Created by Dmitrii Voronin on 25.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
